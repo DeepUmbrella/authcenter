149767e211ae566b81f28afcc88e4e0b9509ce6d
UmbrellaBoy <89843059+DeepUmbrella@users.noreply.github.com>
2025-04-28 09:09:49 +0000

    init commit

---
+++ src/test/java/com/minuo/authcenter/AuthcenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minuo.authcenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthcenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
